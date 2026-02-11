5499b7979a7f3cc3ff8d0d454271ecf31e232f29
karismita_27 <karismitaa27@gmail.com>
2026-02-11 19:36:32 +0530

    logo

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -15,7 +15,7 @@ const nav = [
 function Logo() {
   return (
     <div className="flex items-center">
-      <img src= {mainLogo} alt="Byatikram Group Logo" className="w-28 h-auto"/>
+      <img src= {mainLogo} alt="Byatikram Group Logo" className="w-24 h-auto"/>
       </div>
   );
 }
